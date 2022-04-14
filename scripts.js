38a796191dfadd5a2a881c8c8e60743673fa8478
xtarzyk <xtarzyk@tutanota.com>
2022-04-14 11:49:17 +0200

    final commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const qS = document.querySelectorAll('.block')
+console.log(qS)
+
+const gN = document.getElementsByName('h3')
+console.log(gN)
+
+const gC = document.getElementsByClassName('side-bar')
+console.log(gC)
+
+const gT = document.getElementsByTagName('p')
+console.log(gT)
